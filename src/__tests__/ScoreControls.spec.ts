90b9ab2073fbac79a5296dd2c167f386ab9829b1
Gavin Bannerman <gavinb@administrate.co>
2021-11-07 21:54:27 +0000

    Add tests for ScoreControls component

---
+++ src/__tests__/ScoreControls.spec.ts
@@ -0,0 +1,26 @@
+import {render, fireEvent} from '@testing-library/svelte'
+
+import ScoreControls from '../ScoreControls.svelte';
+
+describe("ScoreControls component", () => {
+  it("renders correctly", () => {
+    const { container } = render(ScoreControls, {
+      onResetClick: () => {}
+    });
+    
+    expect(container).toMatchSnapshot();
+  });
+
+  it("calls onResetClick Reset score button clicked", async () => {
+    const mockOnResetClick = jest.fn();
+
+    const { getByRole } = render(ScoreControls, {
+      onResetClick: mockOnResetClick
+    });
+
+    const resetScoreButton = getByRole("button");
+
+    await fireEvent.click(resetScoreButton);
+    expect(mockOnResetClick).toHaveBeenCalledTimes(1);
+  });
+});
